bfafccdb10d698b9e0b881f7144f766170a97391
piupiunn <ultragalaktikkozmikyokedici@gmail.com>
2022-04-15 23:35:11 +0300

    header upd.

--- components/Header.js
+++ components/Header.js
@@ -5,17 +5,26 @@ import { Search } from "@styled-icons/evaicons-solid/Search";
 import { World } from "@styled-icons/typicons/World";
 import { ThreeBars } from "@styled-icons/octicons/ThreeBars";
 import { UserCircle } from "@styled-icons/boxicons-solid/UserCircle";
+import { PlusCircle } from "@styled-icons/boxicons-regular/PlusCircle";
+import { MinusCircle } from "@styled-icons/boxicons-regular/MinusCircle";
 
 //
-
+import { Popover } from "@mantine/core";
 import { DateRangePicker } from "@mantine/dates";
 import { DatePicker } from "@mantine/dates";
 
 export default function Header() {
   const [value, setValue] = useState([Date | null, Date | null]);
+  const [bigSearchIcon, setBigSearchIcon] = useState(false);
+  const [opened, setOpened] = useState(false);
+
+  const [yetiskinler, setYetiskinler] = useState(0);
+  const [cocuklar, setCocuklar] = useState(0);
+  const [bebekler, setBebekler] = useState(0);
+  const [hayvanlar, setHayvanlar] = useState(0);
 
   return (
-    <header>
+    <header style={{ height: "646.81px" }}>
       <TopInfo>
         <h4>
           COVID-19 çerçevesinde aldığımız önlemler hakkındaki en güncel
@@ -25,18 +34,19 @@ export default function Header() {
       {/* Left */}
 
       <Navbar>
-        <StyledImage
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
-          width={102}
-          height={80}
-          objectFit="contain"
-          objectPosition="left"
-          alt="Airbnb Logo"
-        />
-
+        <LogoDiv>
+          <StyledImage
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png"
+            width={102}
+            height={80}
+            objectFit="contain"
+            objectPosition="left"
+            alt="Airbnb Logo"
+          />
+        </LogoDiv>
         {/* Middle */}
 
-        <DetailedSearchBar>
+        <DetailedSearchBar onClick={(e) => setBigSearchIcon(true)}>
           <Titles>
             <h3>Konaklama yerleri</h3>
             <h3>Deneyimler</h3>
@@ -60,16 +70,80 @@ export default function Header() {
               <h4>Çıkış</h4>
               <p>Tarih ekleyin</p>
             </OutDate>
-            <Guests>
+            <Guests bigSearchIcon={bigSearchIcon}>
               <div>
                 <h4>Misafirler</h4>
                 <p>Misafir ekleyin</p>
               </div>
-              <SearchIcon />
+              <PopoverIcon
+                opened={opened}
+                onClose={() => setOpened(false)}
+                target={<Guests onClick={() => setOpened(true)} />}
+                position="bottom"
+              >
+                <PopoverGuestsDetails>
+                  <GuestsInfo>
+                    <div>
+                      <h5>Yetişkinler</h5>
+                      <p>13 ve üzeri yaştakiler</p>
+                    </div>
+                    <div>
+                      <MinusIcon
+                        onClick={() => setYetiskinler(yetiskinler - 1)}
+                      />
+                      {yetiskinler}
+                      <PlusIcon
+                        onClick={() => setYetiskinler(yetiskinler + 1)}
+                      />
+                    </div>
+                  </GuestsInfo>
+                  <GuestsInfo>
+                    <div>
+                      <h5>Çocuklar</h5>
+                      <p>yaş aralığı 2-12</p>
+                    </div>
+                    <div>
+                      {" "}
+                      <MinusIcon onClick={() => setCocuklar(cocuklar - 1)} />
+                      {cocuklar}
+                      <PlusIcon
+                        onClick={() => setCocuklar(cocuklar + 1)}
+                      />{" "}
+                    </div>
+                  </GuestsInfo>
+                  <GuestsInfo>
+                    <div>
+                      <h5>Bebekler</h5>
+                      <p>2 yaş ve altı</p>
+                    </div>
+                    <div>
+                      <MinusIcon onClick={() => setBebekler(bebekler - 1)} />
+                      {bebekler}
+                      <PlusIcon onClick={() => setBebekler(bebekler + 1)} />
+                    </div>
+                  </GuestsInfo>
+                  <GuestsInfo>
+                    <div>
+                      <h5>Evcil Hayvanlar</h5>
+                      <p style={{ width: "200px" }}>
+                        Yanınızda hizmet hayvanı mı getiriyorsunuz?
+                      </p>
+                    </div>
+                    <div>
+                      <MinusIcon onClick={() => setHayvanlar(hayvanlar - 1)} />
+                      {hayvanlar}
+                      <PlusIcon onClick={() => setHayvanlar(hayvanlar + 1)} />
+                    </div>
+                  </GuestsInfo>
+                </PopoverGuestsDetails>
+              </PopoverIcon>
+              <SearchButton bigSearchIcon={bigSearchIcon}>
+                <SearchIcon /> {bigSearchIcon ? "Arama" : ""}
+              </SearchButton>
             </Guests>
           </Bar>
         </DetailedSearchBar>
-
+        <button onClick={() => setBigSearchIcon(false)}>setFALSe</button>
         {/* Right */}
 
         <RightSection>
@@ -78,10 +152,19 @@ export default function Header() {
 
           <NavRightModal>
             <ThreebarsIcon />
+
             <UserCircleIcon />
           </NavRightModal>
         </RightSection>
       </Navbar>
+
+      <BottomInfo>
+        <h6>
+          Ukrayna'dan kaçan mültecilerden 100.000 kişiyi barındırmamıza yardımcı
+          olun
+        </h6>
+        <NavBottomButton>Daha fazla bilgi edinin</NavBottomButton>
+      </BottomInfo>
     </header>
   );
 }
@@ -97,13 +180,13 @@ const TopInfo = styled.div`
   text-align: center;
 `;
 {
-  /**Parent */
+  /**All-Parent */
 }
 const Navbar = styled.div`
   display: flex;
   position: sticky;
   width: 100%;
-  height: 162px;
+
   padding-left: 21px;
   padding-right: 21px;
   z-index: 50;
@@ -116,9 +199,7 @@ const Navbar = styled.div`
     padding: 0px;
   }
 `;
-{
-  /**Parent */
-}
+
 //
 {
   /**Logo */
@@ -126,12 +207,16 @@ const Navbar = styled.div`
 const StyledImage = styled(Image)`
   cursor: pointer;
 `;
+const LogoDiv = styled.div`
+  margin-bottom: 20px;
+`;
 //
 {
   /**Middle Section */
 }
 const DetailedSearchBar = styled.div`
   margin-left: 10%;
+
   h4,
   p {
     font-size: 13px;
@@ -205,21 +290,38 @@ const Guests = styled.div`
   height: 66px;
   border: 1px solid #ffffff;
   border-radius: 50px;
-
+  display: block;
+  margin-right: ${(props) => (props.bigSearchIcon ? "50px" : "")};
   &:hover {
     background-color: #ebebeb;
   }
   display: flex;
+  justify-content: space-around;
+  div {
+    h4,
+    p {
+      width: 109px;
+    }
+  }
 `;
 //
-const SearchIcon = styled(Search)`
-  background-color: #e11960;
+const SearchButton = styled.button`
   color: white;
-  border-radius: 50%;
-  width: 48px;
+  border-radius: ${(props) => (props.bigSearchIcon ? "20px" : "50%")};
+  width: ${(props) => (props.bigSearchIcon ? "110.14px" : "48px")};
+  background-color: #e11960;
   height: 48px;
-  margin-left: 50px;
-  margin-top: 8px;
+  position: relative;
+  border: none;
+  cursor: pointer;
+  margin-top: 7px;
+  margin-right: ${(props) => (props.bigSearchIcon ? "-32px" : "")};
+`;
+const SearchIcon = styled(Search)`
+  color: white;
+  height: 25px;
+
+  display: inline-block;
 `;
 //
 {
@@ -257,4 +359,85 @@ const UserCircleIcon = styled(UserCircle)`
   width: 30px;
   height: 30px;
   color: #595959;
+`;
+//
+{
+  /**Bottom-Info */
+}
+//
+const BottomInfo = styled.div`
+  width: 1061px;
+  height: 328.81px;
+  padding-left: 80px;
+  padding-right: 80px;
+  background-color: rgb(255, 56, 92);
+  border-radius: 12px;
+  display: grid;
+  grid-template-columns: 1fr;
+  margin-top: 50px;
+  box-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  justify-items: center;
+  h6 {
+    font-size: 38px;
+    color: white;
+    font-weight: 600;
+    text-align: center;
+    margin-bottom: 24px;
+    justify-content: center;
+  }
+`;
+const NavBottomButton = styled.button`
+  width: 174.42px;
+  height: 36px;
+  background-color: #ff385c;
+  padding-right: 16px;
+  padding-left: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
+  border-radius: 8px;
+  cursor: pointer;
+  text-align: center;
+  color: white;
+  font-size: 14px;
+  border: 1px solid white;
+
+  margin-left: 16px;
+  display: inline-block;
+`;
+//
+{
+  /**Popover-Icon */
+}
+//
+const PopoverIcon = styled(Popover)`
+  background-color: red;
+  width: 50px;
+  margin-right: 100px;
+  opacity: 0;
+  position: absolute;
+`;
+
+const PopoverGuestsDetails = styled.div`
+  width: 393px;
+  height: 394px;
+  border-radius: 32px;
+  padding: 16px 32px;
+  display: grid;
+`;
+const GuestsInfo = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const PlusIcon = styled(PlusCircle)`
+  width: 25px;
+  height: 25px;
+  color: gray;
+`;
+const MinusIcon = styled(MinusCircle)`
+  width: 25px;
+  height: 25px;
+  color: gray;
 `;
